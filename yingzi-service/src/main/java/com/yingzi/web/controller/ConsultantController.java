ba8f9006ea457bbf76d55437cead25bfff7e6c35
lkzlee <liyongchao89@gmail.com>
2015-10-13 23:22:37 +0800

    微信端适配

--- yingzi-service/src/main/java/com/yingzi/web/controller/ConsultantController.java
+++ yingzi-service/src/main/java/com/yingzi/web/controller/ConsultantController.java
@@ -404,8 +404,8 @@ public class ConsultantController {
 		cqrDto.setId(consultant_id);
 		cqrDto.setStatus(StatusEnum.ACCEPTED);
 		ConsultantInfo cinfo=consultantFacade.queryOne(cqrDto);
-		if(cinfo==null){
-			//做异常处理
+		if(cinfo!=null){
+			request.setAttribute("cinfo", cinfo);
 		}
 		return response_page;
 	}
